fa0c03bbca4120b3cc9d77845d2eebfb6129188f
noud02 <noud.kerver@hotmail.nl>
2018-02-16 14:16:13 +0100

    update circledisc

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ hook.startListening();
 
 hook.on('ready', () => console.log('Ready!'));
 
+hook.on('request', body => console.log(Buffer.concat(body).toString()));
+
+hook.on('build', (service, body) => {
+    switch (service) {
+    case 'travis': {
+        console.log(body);
+        break;
+    }
+    default: {
+        console.log(`Build complete on ${service}!`);
+    }
+    }
+});
+
